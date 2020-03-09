e0421f8358018b0604326cbdd57cb7b57f318a7d
Cos <marcoscz@yorku.ca>
2020-03-09 17:56:20 -0400

    seperated columns that wont be normalized
    some variables need to be untransformed to make the latex tables in R

--- project2/data_cleaning/data_cleaning.py
+++ project2/data_cleaning/data_cleaning.py
@@ -75,6 +75,9 @@ df['avgPerRating'] = df['aggregateRatings']/df['totalRatings']
 ##Normailze the new response variables and sum them
 
 #normalization
+cols_to_norm = ['avg_views_per_day','avgPerRating', 'comments_per_views', 'related_talks_count']
+#save the normed columns in new columns
+new_cols_to_norm = ['avg_views_per_day_norm','avgPerRating_norm', 'comments_per_views_norm', 'related_talks_count_norm']
 other_cols_to_norm = ['comments',
  'duration',
  'num_speaker',
@@ -83,10 +86,21 @@ other_cols_to_norm = ['comments',
  'film_age',
  'aggregateRatings',
  'totalRatings']
-cols_to_norm = ['avg_views_per_day','avgPerRating', 'comments_per_views', 'related_talks_count']
-#save the normed columns in new columns
-new_cols_to_norm = ['avg_views_per_day_norm','avgPerRating_norm', 'comments_per_views_norm', 'related_talks_count_norm']
+
+#these columns are made so as to make the latex tables for continuous/categorical data 
+#easier to create without affecting the established models
+old_columns_without_norm = ['comments_no_norm',
+ 'duration_no_norm',
+ 'num_speaker_no_norm',
+ 'views_no_norm',
+ 'publication_age_no_norm',
+ 'film_age_no_norm',
+ 'aggregateRatings_no_norm',
+ 'totalRatings_no_norm']
+df[old_columns_without_norm ] = df[other_cols_to_norm]
+#save these normed columns to new columns
 df[new_cols_to_norm]= df[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+#change these columns
 df[other_cols_to_norm]= df[other_cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 df['popularity'] = df[new_cols_to_norm].sum(axis=1)
 
