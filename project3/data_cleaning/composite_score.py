d0ad61cafadee361639d7083a2f659e95d8ae232
Marcos Cardenas-Zelaya <marcos.cardenas-zelaya@mail.mcgill.ca>
2020-03-28 19:29:08 -0400

    finished composite score pipeline

--- project3/data_cleaning/composite_score.py
+++ project3/data_cleaning/composite_score.py
@@ -1,27 +1,31 @@
 import pandas as pd
 
 df = pd.read_csv(r'../data/final_cleaned.csv')
-
 def normalizer_and_composite_score_maker(df, norm_cols_range, composite_score_name):
     """
     This function normalizes the 'proportionalized' columns in df with column number norm_cols_range. Each column is normalized
-    and set to a new column. These columns are then summed to create a composite_score_name
+    and set to a new column. These columns are then summed to create composite_score_name
     """
     new_cols_to_norm = []
     for col in norm_cols_range:
         original_col_name = df.columns[col]
+        col_max = df[original_col_name].max()
+        col_min = df[original_col_name].min()
         normalized_col_name = 'norm_' + original_col_name
         #normalize the old column and write to new column
-        df[normalized_col_name] = df[original_col_name].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+        df[normalized_col_name] = df[original_col_name].apply(lambda x: (x - col_min) / (col_max - col_min))
         new_cols_to_norm.append(normalized_col_name)
+
+    print(list(df))
     df[composite_score_name] = df[new_cols_to_norm].sum(axis=1)
     return df
 
 #initialize the column ranges
 cols_range_human = list(range(30,33)) +list(range(42,43))
 cols_range_econ = list(range(33,42))
-import
+
 df = normalizer_and_composite_score_maker(df, cols_range_human, 'human_cost_comp_score')
 df = normalizer_and_composite_score_maker(df, cols_range_econ, 'econ_cost_comp_score')
 
 
+df.to_csv(r'../data/final_cleaned_with_composite_scores.csv', index = False)
